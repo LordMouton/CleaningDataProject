aca475955ae10e64ea49d8c10471a1196a104d26
Benoit K <bkieffer88@gmail.com>
2017-12-31 14:19:18 +0100

    script updated with binding all measurements

--- run_analysis.R
+++ run_analysis.R
@@ -40,6 +40,9 @@ bind_test_tbl <- bind_cols(X_test_subject_tbl, bind_test_tbl)
 bind_train_tbl <- bind_cols(Y_train_tbl,X_train_tbl)
 bind_train_tbl <- bind_cols(X_train_subject_tbl, bind_train_tbl)
 
+## Merge test and train datasets
+bind_all_tbl <- bind_rows(bind_test_tbl,bind_train_tbl)
+
 # Step 3: Use descriptive activity names to name the activities in the data set
 ## Defining a small function to identify with activity name corresond to which activity number
 ## based on the table in activity_labels.txt
@@ -48,21 +51,19 @@ activity <- function(x) {
 }
 
 ## use this function to create a new column replacing the activity number with activity labels
-bind_test_tbl <- mutate(bind_test_tbl,ActivityLabel = activity(Activity))
-bind_train_tbl <- mutate(bind_train_tbl,ActivityLabel = activity(Activity))
+bind_all_tbl <- mutate(bind_all_tbl,ActivityLabel = activity(Activity))
 
 # Step 4 : Extract only the measurements on the mean and standard deviation for each measurement. 
 ## Select only the variables that are requested : those which contains "mean" or "std"
 ## and columns ActivityLabel and Subject
-bind_test_tbl <- select(bind_test_tbl,Subject,ActivityLabel,contains("mean"),contains("std"))
-bind_train_tbl <- select(bind_train_tbl,Subject,ActivityLabel,contains("mean"),contains("std"))
+bind_all_tbl <- select(bind_all_tbl,Subject,ActivityLabel,contains("mean"),contains("std"))
 
 #Step 5 : Create new independant dataset with average of each variable for each activity and each subject
 ## First, the tbl created above are grouped by Activity and by Subject, then the mean function is applied to
 ## each variable using summarise_all
-avg_test <- bind_test_tbl %>%
+avg_all <- bind_all_tbl %>%
   group_by(Subject,ActivityLabel) %>%
   summarise_all(mean)
-avg_train <- bind_train_tbl %>%
-  group_by(Subject,ActivityLabel) %>%
-  summarise_all(mean)
+
+#Step 6: extract the dataset to a file
+write.csv(avg_all,"./final_avg_dataset.csv",row.names=F)
